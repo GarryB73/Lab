bf451c661d5bec0cce2c403a6fb26cdcfffe2011
GarryB73 <122373091+GarryB73@users.noreply.github.com>
2023-03-27 15:42:10 +0100

    change the way SSMS is installed

--- Windows_Servers/roles/SQL/tasks/Install.ssms.ps1
+++
@@ -1,5 +0,0 @@
-$media_path = "C:\Software\SSMS\SSMS-Setup-ENU.exe"
-$install_path = "$env:SystemDrive\SSMSto"
-$params = "/Install /Quiet SSMSInstallRoot=`"$install_path`""
-
-Start-Process -FilePath $media_path -ArgumentList $params -Wait
